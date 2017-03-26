02939251b6faac260de7ecf88b996ed91665a626
Vince Roti <vince.roti@gmail.com>
2017-03-25 23:03:24 -0500

    two input boxes and connections to peerjs servers

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,12 +6,41 @@ class MessageForm extends React.Component {
     super(props);
   }
 
+  // peerjs() {
+  //   var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  //   getUserMedia({video: true, audio: true}, function(stream) {
+  //     var call = peer.call('another-peers-id', stream);
+  //     call.on('stream', function(remoteStream) {
+  //       // Show stream in some video/canvas element.
+  //     });
+  //   }, function(err) {
+  //     console.log('Failed to get local stream' ,err);
+  //   });
+
+
+  //   var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  //   peer.on('call', function(call) {
+  //     getUserMedia({video: true, audio: true}, function(stream) {
+  //       call.answer(stream); // Answer the call with an A/V stream.
+  //       call.on('stream', function(remoteStream) {
+  //         // Show stream in some video/canvas element.
+  //       });
+  //     }, function(err) {
+  //       console.log('Failed to get local stream' ,err);
+  //     });
+  //   });
+  // }
+
   componentDidMount() {
+    var peer = new Peer({key: '72su953vnzcqsemi'}); // imported in head of HTML, need to refactor for webpack
+    peer.on('open', function(id) {
+      console.log('My peer ID is: ' + id);
+    });
+
     var that = this;
     navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-    var constraints = { video: true };
     function successCallback(localMediaStream) {
       window.stream = localMediaStream; // stream available to console
       let video = that.refs.localVideo;
@@ -23,7 +52,7 @@ class MessageForm extends React.Component {
       console.log('navigator.getUserMedia error: ', error);
     }
 
-    navigator.getUserMedia(constraints, successCallback, errorCallback);
+    navigator.getUserMedia({ video: true, }, successCallback, errorCallback);
   }
 
   componentWillUnmount() {
@@ -34,6 +63,8 @@ class MessageForm extends React.Component {
 
     return (
       <div>
+        <input className="video-input" placeholder="Your ID"ref='localId'/>
+        <input className="video-input" placeholder="Connection ID" ref='remoteId'/>
         <video ref="localVideo" autoPlay></video>
         <h2>Currently only displays local cam feedback</h2>
       </div>
