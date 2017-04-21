c56b0e9af00282631eb98bd00921ed3329d409cc
Vince Roti <vince.roti@gmail.com>
2017-04-21 18:16:51 -0500

    added homepage content and used bootstraps .container

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,11 @@ class Home extends React.Component {
 
   render() {
     return(
-      <div>
-        <p>Test</p>
+      <div className="container">
+        <h1>React Live Chat</h1>
+        <hr/>
+        <h3> This is an app I created using ReactJS, React Router, Node, Express, WebRTC, and MongoDB. </h3>
+        <h3> Video chat uses WebRTC and a p2p connection to connect and the live chat uses websockets. All chat information is backed up to MongoDB. </h3>
       </div>
     );
   }
